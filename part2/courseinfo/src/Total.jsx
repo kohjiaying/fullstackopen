77a0b24138830538bd8c5623d872b895900eb898
kohjiaying <73278030+kohjiaying@users.noreply.github.com>
2024-02-01 15:51:06 +0800

    Delete part2/courseinfo directory

--- part2/courseinfo/src/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({parts}) => {
-    console.log('in Total', parts)
-    const sum = parts.reduce((total, obj) => obj.exercises + total,0)
-    return (
-        <p>total of {sum} exercises</p>
-    )
-  } 
-  
-  export default Total
